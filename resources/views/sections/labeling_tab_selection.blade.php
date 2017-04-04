e35384a39fd24b03737ecf59e66cbe031149c1d2
elicul <enzo.licul@yahoo.com>
2017-04-04 22:46:14 +0200

    Circular slider finished

--- resources/views/sections/labeling_tab_selection.blade.php
+++ resources/views/sections/labeling_tab_selection.blade.php
@@ -2,24 +2,39 @@
 <div class="uk-grid" data-uk-grid-margin>
 	<div class="uk-width-large-1-10"></div>
 	<div class="uk-width-large-8-10">
-
-			<div class="uk-slidenav-position" data-uk-slider="{infinite: false}">
+			<div class="uk-grid" data-uk-grid-margin>
+				<div class="uk-width-large-2-10"></div>
+				<div class="uk-width-large-6-10" >
+					<h1 style="text-align: center; margin-top: 50px; ">Labeling Aplications</h1>
+				</div>
+				<div class="uk-width-large-2-10"></div>
+			</div>
+			<div class="uk-slidenav-position" data-uk-slider="{infinite: false}" style="height: inherit;">
 				<div class="uk-slider-container">
 					<div class="uk-grid" data-uk-grid-margin>
 						<div class="uk-width-large-2-10"></div>
 						<div class="uk-width-large-6-10" style="padding-top: 50px; padding-bottom: 50px;">
-							<!--<h1 style="text-align: center; margin-top: 20px; margin-bottom: 20px;">Labeling Aplications</h1>-->
 							<ul class="uk-slider uk-grid uk-grid-width-medium-1-5" style="min-width: 1715.5px; min-height: 25px; transform: translateX(-428.875px);">
-								<li data-slider-slide="0" style="left: 0px; width: 214.438px;" class="uk-slide-before hvr-grow">
-									<img src="{{asset('images/labeling/pressure_sensitive.png')}}" width="600" height="400" alt="1 [600x400]" draggable="false" data-holder-rendered="true"></li>
-								<li data-slider-slide="1" style="left: 214.438px; width: 214.438px;" class="uk-slide-before hvr-grow">
-									<img src="{{asset('images/labeling/hot_melt.png')}}" width="600" height="400" alt="2 [600x400]" draggable="false" data-holder-rendered="true"></li>
-								<li data-slider-slide="2" style="left: 428.875px; width: 214.438px;" class="hvr-grow">
-									<img src="{{asset('images/labeling/wet_labels.png')}}" width="600" height="400" alt="3 [600x400]" draggable="false" data-holder-rendered="true"></li>
-								<li data-slider-slide="3" style="left: 643.313px; width: 214.438px;" class="uk-slide-after hvr-grow">
-									<img src="{{asset('images/labeling/magnetic_strip.png')}}" width="600" height="400" alt="4 [600x400]" draggable="false" data-holder-rendered="true"></li>
-								<li data-slider-slide="4" style="left: 857.75px; width: 214.438px;" class="uk-slide-after hvr-grow">
-									<img src="{{asset('images/labeling/3d_labels.png')}}" width="600" height="400" alt="5 [600x400]" draggable="false" data-holder-rendered="true"></li>
+								<li data-slider-slide="0" style="left: 0px; width: 214.438px; text-align: center; text-transform: uppercase;" class="uk-slide-before hvr-grow">
+									<img src="{{asset('images/labeling/pressure_sensitive.png')}}" width="600" height="400" alt="1 [600x400]" draggable="false" data-holder-rendered="true" style="margin-bottom: 10px;">
+									Pressure Sensitive
+								</li>
+								<li data-slider-slide="1" style="left: 214.438px; width: 214.438px; text-align: center; text-transform: uppercase;" class="uk-slide-before hvr-grow">
+									<img src="{{asset('images/labeling/hot_melt.png')}}" width="600" height="400" alt="2 [600x400]" draggable="false" data-holder-rendered="true" style="margin-bottom: 10px;">
+									Hot melt
+								</li>
+								<li data-slider-slide="2" style="left: 428.875px; width: 214.438px; text-align: center; text-transform: uppercase;" class="hvr-grow">
+									<img src="{{asset('images/labeling/wet_labels.png')}}" width="600" height="400" alt="3 [600x400]" draggable="false" data-holder-rendered="true" style="margin-bottom: 10px;">
+									Wet labels
+								</li>
+								<li data-slider-slide="3" style="left: 643.313px; width: 214.438px; text-align: center; text-transform: uppercase;" class="uk-slide-after hvr-grow">
+									<img src="{{asset('images/labeling/magnetic_strip.png')}}" width="600" height="400" alt="4 [600x400]" draggable="false" data-holder-rendered="true" style="margin-bottom: 10px;">
+									Magnetic strip
+								</li>
+								<li data-slider-slide="4" style="left: 857.75px; width: 214.438px; text-align: center; text-transform: uppercase;" class="uk-slide-after hvr-grow">
+									<img src="{{asset('images/labeling/3d_labels.png')}}" width="600" height="400" alt="5 [600x400]" draggable="false" data-holder-rendered="true" style="margin-bottom: 10px;">
+									3d labels
+								</li>
 								<!--
 								<li data-slider-slide="5" style="left: 1072.19px; width: 214.438px;" class="uk-slide-after">
 								<img src="" width="600" height="400" alt="6 [600x400]" draggable="false" data-src="holder.js/600x400/lava/auto/text:6/size:100" data-holder-rendered="true"></li>
@@ -29,8 +44,8 @@
 								<img src="" width="600" height="400" alt="8 [600x400]" draggable="false" data-src="holder.js/600x400/vine/auto/text:8/size:100" data-holder-rendered="true"></li>
 							-->
 							</ul>
-							<a href="#" class="uk-slidenav uk-slidenav-contrast-labeling uk-slidenav-previous uk-slidenav-labeling" style="left:200px;" data-uk-slider-item="previous" draggable="false"></a>
-							<a href="#" class="uk-slidenav uk-slidenav-contrast-labeling uk-slidenav-next uk-slidenav-labeling" style="right:200px;" data-uk-slider-item="next" draggable="false"></a>
+							<a href="#" class="uk-slidenav uk-slidenav-contrast-labeling uk-slidenav-previous uk-slidenav-labeling" style="left:200px; top: 40%;" data-uk-slider-item="previous" draggable="false"></a>
+							<a href="#" class="uk-slidenav uk-slidenav-contrast-labeling uk-slidenav-next uk-slidenav-labeling" style="right:200px; top: 40%;" data-uk-slider-item="next" draggable="false"></a>
 						</div>
 						<div class="uk-width-large-2-10"></div>
 					</div>
@@ -38,6 +53,8 @@
 				<ul class="uk-dotnav uk-dotnav-contrast-labeling uk-position-bottom uk-flex-center">
 					<li data-uk-slideshow-item="0"><a href=""></a></li>
 					<li data-uk-slideshow-item="1"><a href=""></a></li>
+					<li data-uk-slideshow-item="2"><a href=""></a></li>
+					<li data-uk-slideshow-item="3"><a href=""></a></li>
 				</ul>
 			</div>
 	</div>

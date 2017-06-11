00eb069b4a3c1bf4b4cdc73231b033f15178122e
Marko Dupor <marko.dupor@gmail.com>
2017-06-11 17:47:02 +0200

    before bootstrap change

--- resources/views/sections/technologies_labeling.blade.php
+++ resources/views/sections/technologies_labeling.blade.php
@@ -1,22 +1,25 @@
 <section id="section-technologies_labeling" class="section section--indented">
     <div class="uk-grid uk-grid-collapse uk-grid-width-1-3 image--infinite-left-1-3">
         <div style="background-image:url('{{asset('images/technologies_labeling.png')}}');background-repeat: no-repeat;
-    background-size: contain;">
+                background-size: contain;">
         </div>
     </div>
     <div class="uk-container uk-container-center">
         <div class="section__inside">
             <div class="uk-grid">
-                <img data-wt-height="684" data-wt-width="960" data-wt-image-ratio="NaN" src="{{asset('images/technologies_labeling.png')}}" class="technologies_labeling_img">
+                <img data-wt-height="684" data-wt-width="960" data-wt-image-ratio="NaN"
+                     src="{{asset('images/technologies_labeling.png')}}" class="technologies_labeling_img">
                 <div class="  uk-width-medium-1-3 uk-width-large-1-3 uk-hidden-small">&nbsp;</div>
                 <div class="  uk-width-medium-2-3 uk-width-large-2-3">
-                  <h2 class="technologies-tab-h2">{{$title}}</h2>
+                    <h2 class="technologies-tab-h2">{{$title}}</h2>
                     <h3>Lorem ipsum dolor sit amet</h3>
                     &nbsp;
                     <div class="uk-grid" style="margin-bottom: 50px; ">
                         <p class="paragraph paragraph--muted">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a sapien quis lorem rutrum varius.
-                            Cras ultricies sit amet sem nec efficitur. In dictum placerat malesuada. Sed tempor maximus auctor
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a sapien quis lorem rutrum
+                            varius.
+                            Cras ultricies sit amet sem nec efficitur. In dictum placerat malesuada. Sed tempor maximus
+                            auctor
                         </p>
                     </div>
                     <div class="button__group">
@@ -29,4 +32,128 @@
             </div>
         </div>
     </div>
+</section>
+
+
+<section id="section-technologies_labeling_tabs" class="section section--indented">
+
+    <div class="uk-container uk-container-center">
+        <div class="section__inside">
+            <div class="uk-grid">
+
+                <div class="row" >
+                    <!-- start: TABS -->
+                    <ul class="nav nav-tabs">
+                        <li class="active">
+                            <a data-toggle="tab" href="#overview">
+                                <strong>Overview</strong>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-toggle="tab" href="#specifications">
+                                <strong>Specifications</strong>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-toggle="tab" href="#applications">
+                                <strong>Applications</strong>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-toggle="tab" href="#related_modules">
+                                <strong>Related Modules</strong>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-toggle="tab" href="#brochures">
+                                <strong>Brochures</strong>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-toggle="tab" href="#videos">
+                                <strong>Videos</strong>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div class="tab-content panel" style="border-radius: 1px">
+                        <div id="overview" class="tab-pane fade active in">
+
+                            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                                the picture goes here
+                            </div>
+
+                            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                    left column of text
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                    right column of text
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div id="specifications" class="tab-pane fade" style="overflow-x:auto;max-height:292px;padding: 30px;">
+
+                        </div>
+
+                        <div id="applications" class="tab-pane fade" style="overflow-x:auto;max-height:292px;padding: 30px;">
+
+                            <div class="col-lg-4">
+
+                            </div>
+                            <div class="col-lg-4">
+
+                            </div>
+                            <div class="col-lg-4">
+
+                            </div>
+                        </div>
+
+                        <div id="related_modules" class="tab-pane fade" style="overflow-x:auto;max-height:292px;padding: 30px;">
+
+                            <div class="row">
+                                <div class="col-lg-3">
+
+                                </div>
+                                <div class="col-lg-3">
+
+                                </div>
+                                <div class="col-lg-3">
+
+                                </div>
+                                <div class="col-lg-3">
+
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-3">
+
+                                </div>
+                                <div class="col-lg-3">
+
+                                </div>
+                                <div class="col-lg-3">
+
+                                </div>
+                                <div class="col-lg-3">
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="brochures" class="tab-pane fade" style="overflow-x:auto;max-height:292px;padding: 30px;">
+
+                        </div>
+
+                        <div id="videos" class="tab-pane fade" style="overflow-x:auto;max-height:292px;padding: 30px;">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </section>
